002032eb80bf32cb03677adb10d6b7381e5ca552
adricosmicfox <adriiphone@gmail.com>
2022-09-12 16:08:39 +0200

    created html and js with completed exercise and bonus

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const vueElement = new Vue({
+    el: ".text-box",
+    data: {
+        userName: "Adriano",
+        userSurname: "Massimei",
+        userImage: "https://64.media.tumblr.com/921befb7a3b7850995d36fd97d439412/tumblr_o1dj8v3XSM1v39f3co1_500.gifv",
+    }
+
+})
